7dd994ba41f1ebcb5905d7b62415d4617b6afc78
JoseVicente1988 <josevicentevicedosilla@gmail.com>
2025-10-16 07:23:57 +0200

    changes

--- clear.bat
+++ clear.bat
@@ -1,9 +1,2 @@
-python -m pip install git-filter-repo
-
-REM Elimina todo node_modules y .next del historial
-git filter-repo --path node_modules --path .next --invert-paths
-
-REM Alternativa más fina: solo los binarios SWC y .next
-REM git filter-repo --path-glob "node_modules/@next/swc-*/**" --path .next --invert-paths
-
-git push origin --force
+rmdir /s /q prisma\migrations
+del prisma\migration_lock.toml 2>nul
